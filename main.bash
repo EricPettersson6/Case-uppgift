f12c57a8e6537accbf2d25e18c54d2d81f73dd6a
Casper <casper.hjorth4@gmail.com>
2024-12-25 23:36:19 +0000

    Added info and started to work on the main menu

--- main.bash
+++ main.bash
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+#
+#Skapad av Casper Hjorth och Eric Pettersson, grupp 32
+#
+# Licens: MIT License
+#
+# Motivering: vi valde MIT license för att (skriv motivering här)
+#
+
+# Kontrollera om skriptet körs som root
+if [[ $EUID -ne 0 ]]; then
+	echo "Detta skript måste köras som  root (sudo). Avslutar."
+	exit 1
+fi
+
+# Funktion för att visa huvudmenyn
+show_mainmenu() {
+	clear
+	echo "================================="
+	echo "	SYSTEM MANAGER (version 1.0.0) "
+	echo "---------------------------------"
